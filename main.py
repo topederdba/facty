78604caa12f4d601eb9851f0ca103737dbb7fde2
topederdba <138445515+topederdba@users.noreply.github.com>
2023-07-03 10:45:27 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+// Prompt the user to enter a number
+const num = parseInt(prompt("Enter a number:"));
+
+// Calculate the factorial
+const result = factorial(num);
+
+// Display the result
+console.log(`The factorial of ${num} is ${result}`);
